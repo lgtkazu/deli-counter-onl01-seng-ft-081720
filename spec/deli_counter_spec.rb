db62b65059f72b08144450602db809655cd638c6
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-26 15:38:18 +0000

    Done.

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -16,8 +16,8 @@ describe 'Deli Counter' do
 
     context "there are people in line" do
       it "should display the current line" do
-        expect($stdout).to receive(:puts).with("The line is currently: 1. Logan 2. Avi 3. Spencer")
-        line(other_deli)
+   	      expect($stdout).to receive(:puts).with("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
+	line(another_deli)
       end
 
       it 'works with different people in line' do
